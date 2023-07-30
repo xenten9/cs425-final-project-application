1bcd8442ef325fae882fea3f138767d4e1f90d6f
xenten9 <xenten9@gmail.com>
2023-07-30 02:44:28 -0500

    First commit.

---
+++ constants.py
@@ -0,0 +1,12 @@
+SMALL_PAD = {"padx": 1, "pady": 1}
+FILL_X = {"sticky": "ew"}
+FILL_Y = {"sticky": "ns"}
+FILL_BOTH = {"sticky": "ewns"}
+#LEFT_ALIGN = {"sticky": "w"}
+#RIGHT_ALIGN = {"sticky": "e"}
+LEFT_ALIGN = {"anchor": "w"}
+RIGHT_ALIGN = {"anchor": "e"}
+FONT_SMALL = {"font": ("consolas", 18)}
+FONT_MEDIUM = {"font": ("consolas", 24)}
+FONT_LARGE = {"font": ("consolas", 48)}
+
